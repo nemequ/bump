nemequ/bump

commit e6e843616948c28766325f3b3439a79d5f212e31

Semaphore: add fast path to sync lock for when there is no contention
A simple lock/unlock loop takes about 1/20th the time it did before.

bump/bump-semaphore.vala
@@ -159,9 +159,18 @@ namespace Bump {
      * @see lock_async
      */
     public void @lock (int priority = GLib.Priority.DEFAULT, GLib.Cancellable? cancellable = null) throws GLib.Error {
-      // TODO: easy to optimize this a bit for when there is no
-      // resource contention.
       GLib.Mutex mut = GLib.Mutex ();
+
+      if ( this.queue.length == 0 ) {
+        this.mutex.lock ();
+        if ( this.queue.length == 0 ) {
+          this.claims++;
+          this.mutex.unlock ();
+          return;
+        }
+        this.mutex.unlock ();
+      }
+
       mut.lock ();
 
       this.add_without_unlock (() => {

commit 586f5ff2fd91a361d3c1904ba2d4da596b61b941

Semaphore: fix bug introduced in e6e84361

bump/bump-semaphore.vala
@@ -161,9 +161,9 @@ namespace Bump {
     public void @lock (int priority = GLib.Priority.DEFAULT, GLib.Cancellable? cancellable = null) throws GLib.Error {
       GLib.Mutex mut = GLib.Mutex ();
 
-      if ( this.queue.length == 0 ) {
+      if ( this.queue.length == 0 && this.claims < this.max_claims ) {
         this.mutex.lock ();
-        if ( this.queue.length == 0 ) {
+        if ( this.queue.length == 0 && this.claims < this.max_claims ) {
           this.claims++;
           this.mutex.unlock ();
           return;